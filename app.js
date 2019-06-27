1a951521556cbbd3bcf1cde3106d6e1ebed1895d
Xavier Olivares <xm.olivares@gmail.com>
2019-06-26 20:36:48 -0400

    after setup

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "./public")))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+const layout = require('./views/layout')
+
+app.get('/', (req, res) => {
+    // res.send()
+    res.send(layout('Hello World'));
+})
+
+module.exports = app
